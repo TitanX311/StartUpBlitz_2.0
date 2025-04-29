8970126d25b02579a88d64c481efbd7c139eb411
Abir Debnath <abirdebnath311@hotmail.com>
2025-04-29 22:00:13 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ...
+    build: {
+        rollupOptions: {
+          input: {
+            main: './index.html',  // Your main page
+            signin: './signin.html', // Added login page
+          },
+        },
+      }
 })
